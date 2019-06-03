5f0d1dff141a447884fdddf5222683243d63c2ac
Ericwyn <ericwyn.chen@gmail.com>
2019-06-03 11:27:18 +0800

    feat: 添加了一个新的 DataReader 解析方法，试图提升 DataReader 反射解析的效率

--- Corm/corm/utils/SqlDataReaderParse.cs
+++ Corm/corm/utils/SqlDataReaderParse.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
+using System.Reflection;
+using System.Reflection.Emit;
 using CORM.attrs;
 
 namespace CORM.utils
@@ -19,7 +22,8 @@ namespace CORM.utils
             
             while(reader.Read()) {
                 var objTemp = new T();
-                foreach (var property in typeof(T).GetProperties())
+                
+                foreach (PropertyInfo property in typeof(T).GetProperties())
                 {
                     try
                     {
@@ -72,6 +76,104 @@ namespace CORM.utils
             }
             return resList;
         }
+
+        public static List<T> NewParse(SqlDataReader reader, bool closeReader, bool noCloumn)
+        {
+            var resList = new List<T>();
+            var builder = IDataReaderEntityBuilder<T>.CreateBuilder(reader, noCloumn);
+            while (reader.Read())
+            {
+                var entity = builder.Build(reader);
+                resList.Add(entity);
+            }
+            if (closeReader)
+            {
+                reader.Close();
+            }
+            return resList;
+        }
         
+    }
+    
+    
+    public class IDataReaderEntityBuilder<Entity>
+    {
+        private static readonly MethodInfo getValueMethod = typeof(IDataRecord).GetMethod("get_Item", new Type[] { typeof(int) });
+        private static readonly MethodInfo isDBNullMethod = typeof(IDataRecord).GetMethod("IsDBNull", new Type[] { typeof(int) });
+        private delegate Entity Load(IDataRecord dataRecord);
+ 
+        private Load handler;
+        private IDataReaderEntityBuilder() { }
+ 
+        public Entity Build(IDataRecord dataRecord) { return handler(dataRecord); }
+
+        // 新的解析工具类
+        // 参考 “利用反射将IDataReader读取到实体类中效率低下的解决办法”
+        //     https://blog.csdn.net/lilong_herry/article/details/79993907 
+        public static IDataReaderEntityBuilder<Entity> CreateBuilder(IDataRecord dataRecord, bool noCloumn)
+        {
+            IDataReaderEntityBuilder<Entity> dynamicBuilder = new IDataReaderEntityBuilder<Entity>();
+            DynamicMethod method = new DynamicMethod("IDataReaderDynamicCreateEntity", typeof(Entity), new Type[] { typeof(IDataRecord) }, typeof(Entity), true);
+            ILGenerator generator = method.GetILGenerator();
+            LocalBuilder result = generator.DeclareLocal(typeof(Entity));
+            generator.Emit(OpCodes.Newobj, typeof(Entity).GetConstructor(Type.EmptyTypes));
+            generator.Emit(OpCodes.Stloc, result);
+ 
+            // 反射一次拿到所有属性存入一个 Map 里面
+            Dictionary<string, PropertyInfo> propertyMap = new Dictionary<string , PropertyInfo>();
+            foreach (PropertyInfo property in typeof(Entity).GetProperties())
+            {
+                // 没有 Column 注解的话，ColumnName 要等于 Property 的 Name 才可以
+                if (noCloumn)
+                {
+                    propertyMap.Add(property.Name, property);
+                    
+                }
+                // 有 Column 注解的话，columnName 要等于 column 注解的 Name
+                else
+                {
+                    var objAttrs = property.GetCustomAttributes(typeof(Column), true);
+                    if (objAttrs.Length > 0)
+                    {
+                        propertyMap.Add((objAttrs[0] as Column).Name, property);
+                    }
+                }
+            }
+            
+            for (int i = 0; i < dataRecord.FieldCount; i++)
+            {
+                PropertyInfo propertyInfo = null;
+                try
+                {
+                    Console.WriteLine(dataRecord.GetName(i));
+                    propertyInfo = propertyMap[dataRecord.GetName(i)];
+                }
+                catch (System.Collections.Generic.KeyNotFoundException e)
+                {
+                    propertyInfo = null;
+                }
+                // 从 propertyInfo 里面找到与名字一样的 Reader 的 key
+//                PropertyInfo propertyInfo = typeof(Entity).GetProperty(properties.FirstOrDefault(x=>(((Column)x.GetCustomAttributes(typeof(Column), true)[0]).Name!=null?).Equals(dataRecord.GetName(i)))?.Name);
+                Label endIfLabel = generator.DefineLabel();
+                if (propertyInfo != null && propertyInfo.GetSetMethod() != null)
+                {
+                    generator.Emit(OpCodes.Ldarg_0);
+                    generator.Emit(OpCodes.Ldc_I4, i);
+                    generator.Emit(OpCodes.Callvirt, isDBNullMethod);
+                    generator.Emit(OpCodes.Brtrue, endIfLabel);
+                    generator.Emit(OpCodes.Ldloc, result);
+                    generator.Emit(OpCodes.Ldarg_0);
+                    generator.Emit(OpCodes.Ldc_I4, i);
+                    generator.Emit(OpCodes.Callvirt, getValueMethod);
+                    generator.Emit(OpCodes.Unbox_Any, propertyInfo.PropertyType);
+                    generator.Emit(OpCodes.Callvirt, propertyInfo.GetSetMethod());
+                    generator.MarkLabel(endIfLabel);
+                }
+            }
+            generator.Emit(OpCodes.Ldloc, result);
+            generator.Emit(OpCodes.Ret);
+            dynamicBuilder.handler = (Load)method.CreateDelegate(typeof(Load));
+            return dynamicBuilder;
+        }
     }
 }
